cf9a8ba4f917055b4daf2ffcd96953cb2ff0df7e
liw0512 <158794377+liw0512@users.noreply.github.com>
2026-08-12 14:26:07 +0800

    Export centralized plant config

--- system/model/config/__init__.py
+++ system/model/config/__init__.py
@@ -1,3 +1,8 @@
+from .plant_config import PLANT_CONFIG
 from .process4map_config import PROCESS4MAP_CONFIG, Process4MapControlConfig
 
-__all__ = ['PROCESS4MAP_CONFIG', 'Process4MapControlConfig']
+__all__ = [
+    'PLANT_CONFIG',
+    'PROCESS4MAP_CONFIG',
+    'Process4MapControlConfig',
+]
