6f9d449de272ddf7b23d3955315ec7e46513f857
liw0512 <158794377+liw0512@users.noreply.github.com>
2026-08-13 10:24:35 +0800

    Export fast change detector

---
+++ system/model/map_control/fast_change_mode/__init__.py
@@ -0,0 +1,19 @@
+"""独立 FAST_CHANGE 风险识别模块。"""
+
+from .fast_change_config import FAST_CHANGE_CONFIG
+from .fast_change_mode_detector import (
+    FAST_CHANGE,
+    FAST_RECOVERY,
+    REGULAR,
+    FastChangeConfigurationError,
+    FastChangeModeDetector,
+)
+
+__all__ = [
+    "FAST_CHANGE_CONFIG",
+    "FastChangeModeDetector",
+    "FastChangeConfigurationError",
+    "REGULAR",
+    "FAST_CHANGE",
+    "FAST_RECOVERY",
+]
